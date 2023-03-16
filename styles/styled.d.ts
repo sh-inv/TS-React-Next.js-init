2e7e42386c3c325a4ebc3f34207f647193354a51
sh.inv <qierapu1im@gmail.com>
2023-03-16 15:31:38 +0900

    init

---
+++ styles/styled.d.ts
@@ -0,0 +1,9 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaltTheme {
+    colors: {
+      var1: string;
+    };
+  }
+}
---
+++ styles/theme.ts
@@ -0,0 +1,7 @@
+import { DefaltTheme } from 'styled-components';
+
+export const theme: DefaltTheme = {
+  colors: {
+    var1: '#010101',
+  },
+};
